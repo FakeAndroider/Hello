a9a3207542404c0c6a3f29606ce6a2b3e4d5632f
高俊伟 <gjw_2007@163.com>
2024-05-12 20:54:38 +0800

    init project

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+项目：
+Gradle 8.4
+Java 17
+MineSDK 21
+TargetSDK 34
---
+++ buildSrc/src/main/java/Define.kt
@@ -0,0 +1,12 @@
+/**
+ * 相关定义
+ * https://developer.android.google.cn/studio/projects/android-library?hl=zh-cn
+ * https://blog.csdn.net/Welcome_Word/article/details/131968778
+ * */
+object Define {
+    const val TARGET_SDK = 34
+    const val MIN_SDK = 21
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0.0"
+    const val APPLICATION_ID = "org.work"
+}
