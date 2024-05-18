4abf9990860cde3dc7615339a4ad610c5320ef85
gaojunwei <gjw_2007@163.com>
2024-05-18 12:27:11 +0800

    1、navigation类的运用 2、更改项目的    AGP 7.4.2    Gradle 7.5

--- ReadMe.md
+++ ReadMe.md
@@ -1,5 +1,6 @@
 项目：
-Gradle 8.5
-Java 17
-MineSDK 21
+AGP 7.4.2
+Gradle 7.5
+Java 11
+MinSDK 21
 TargetSDK 34
--- app/src/main/java/org/work/MainActivity.java
+++ app/src/main/java/org/work/MainActivity.java
@@ -1,26 +1,28 @@
 package org.work;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 import org.work.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        org.work.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -32,6 +34,26 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        addNavigationItemTapListener(navView, navController);
+    }
+
+    private void addNavigationItemTapListener(final BottomNavigationView navView, final NavController navController) {
+        navView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_home:
+                        navController.navigate(R.id.navigation_home);
+                        return true;
+                    case R.id.navigation_dashboard:
+                        navController.navigate(R.id.navigation_dashboard);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
 }
